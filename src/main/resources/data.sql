2d0d2b30837110252f9ee979bc1d2a57f8d394e3
Valerioscif <valerio.scifoni97@gmail.com>
2023-11-21 14:44:49 +0100

    Modificato data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,20 @@
-INSERT INTO prezzo_settore_evento (id_settore, id_evento, prezzo, is_cancellato) VALUES
-(1, 1, 20.0, false),
-(1, 2, 25.0, false),
-(2, 1, 18.0, false),
-(2, 2, 22.0, false),
-(3, 1, 30.0, false);
+INSERT INTO PrezzoSettoreEvento (evento_id, settore_id, prezzo, is_Cancellato, version) 
+VALUES (1, 1, 30.0, false, 1);
+INSERT INTO PrezzoSettoreEvento (evento_id, settore_id, prezzo, is_Cancellato, version) 
+VALUES (2, 2, 25.5, false, 1);
+INSERT INTO PrezzoSettoreEvento (evento_id, settore_id, prezzo, is_Cancellato, version) 
+VALUES (3, 3, 15.0, false, 1);
+INSERT INTO PrezzoSettoreEvento (evento_id, settore_id, prezzo, is_Cancellato, version) 
+VALUES (4, 4, 20.0, false, 1);
+INSERT INTO PrezzoSettoreEvento (evento_id, settore_id, prezzo, is_Cancellato, version) 
+VALUES (5, 5, 18.5, false, 1);
+INSERT INTO PrezzoSettoreEvento (evento_id, settore_id, prezzo, is_Cancellato, version) 
+VALUES (6, 6, 12.0, false, 1);
+INSERT INTO PrezzoSettoreEvento (evento_id, settore_id, prezzo, is_Cancellato, version) 
+VALUES (7, 7, 25.0, false, 1);
+INSERT INTO PrezzoSettoreEvento (evento_id, settore_id, prezzo, is_Cancellato, version) 
+VALUES (8, 8, 22.5, false, 1);
+INSERT INTO PrezzoSettoreEvento (evento_id, settore_id, prezzo, is_Cancellato, version) 
+VALUES (9, 9, 28.0, false, 1);
+INSERT INTO PrezzoSettoreEvento (evento_id, settore_id, prezzo, is_Cancellato, version) 
+VALUES (10, 10, 30.0, false, 1);
